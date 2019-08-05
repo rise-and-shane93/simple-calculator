cc9a149e997459c00ffb43dbdae044d48c78470c
rise_and_shane93 <cdtharper@gmail.com>
2019-08-05 18:13:31 -0400

    add css

---
+++ src/components/Screen.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Screen extends Component {
+    render() {
+        return(
+            <article id="screen">
+                <h2 className="screenText"></h2>
+            </article>
+        );
+    }
+}
+
+export default Screen;
