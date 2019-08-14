86a4408d6f8f0e0f7fa651e4138db5f328e22fed
rise_and_shane93 <cdtharper@gmail.com>
2019-08-14 07:37:07 -0400

    add percent functionality

--- src/reducers/calculate.js
+++ src/reducers/calculate.js
@@ -158,6 +158,39 @@ export default function addNum(state=initialState, action) {
                         operatorSymbol: '/'
                     }
                 }
+            } else if (action.symbol === '%') {
+                if (state.firstCalc) {
+                    sum = resNum / currNum; 
+                    if (typeof state.result === 'string') {
+                        result = state.currentItem;
+                    } else {
+                        result = state.result;
+                    }
+                    return {
+                        result: result,
+                        currentItem: '',
+                        displayNum: state.result,
+                        firstCalc: !state.firstCalc,
+                        operatorSymbol: '%'
+                    }    
+                } else {
+                    if (state.operatorSymbol === '+') {
+                        sum = resNum + currNum;
+                    } else if (state.operatorSymbol === '-') {
+                        sum = resNum - currNum;
+                    } else if (state.operatorSymbol === 'x') {
+                        sum = resNum * currNum;
+                    } else if (state.operatorSymbol === '/') {
+                        sum = resNum / currNum;
+                    }
+                    return {
+                        result: sum,
+                        currentItem: '',
+                        displayNum: sum,
+                        firstCalc: state.firstCalc,
+                        operatorSymbol: '/'
+                    }
+                }
             }
         }
         case calcBtns.EQUAL_BUTTON: {
