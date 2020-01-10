4347bf620feaa7c29a2776e826665e275f19e98c
rise_and_shane93 <cdtharper@gmail.com>
2020-01-10 18:33:02 -0500

    Updates

---
+++ precache-manifest.5cee137c64396094141e4159e84e0437.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa549728f191063c57209b2ddb3222f1",
+    "url": "/simple-calculator/index.html"
+  },
+  {
+    "revision": "4cc40fc22f63cae9069a",
+    "url": "/simple-calculator/static/css/main.a73e3d90.chunk.css"
+  },
+  {
+    "revision": "baeda0a38b0dcea06342",
+    "url": "/simple-calculator/static/js/2.f1cd2b18.chunk.js"
+  },
+  {
+    "revision": "dd30ad2c00174e59bb32191ea757f584",
+    "url": "/simple-calculator/static/js/2.f1cd2b18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4cc40fc22f63cae9069a",
+    "url": "/simple-calculator/static/js/main.0e0916d9.chunk.js"
+  },
+  {
+    "revision": "29acda98fbd18541556b",
+    "url": "/simple-calculator/static/js/runtime-main.7687f026.js"
+  }
+]);
